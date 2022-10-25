ed776ea1b88f6f284f18f52239a1033a43510094
100rab100 <99138605+100rab100@users.noreply.github.com>
2022-10-25 07:23:29 +0530

    Update for loop.java

---
+++ for loop.java
@@ -0,0 +1,36 @@
+import java.lang.*;
+import java.util.*;
+
+class first
+{
+    public static void main(String args[]) {
+        for (int i=10;i>0;--i)
+        {
+            System.out.println(i);
+        }
+
+    }
+}
+
+
+// second 
+
+package looppractice1;
+
+public class LoopPractice1 {
+
+    public static void main(String[] args) {
+        //for(int i=0;i<=10;i++)
+        //for(int i=0;i>0;i--)
+        
+        //int i=0;
+        //for(System.out.println("Hi");i<=10;i++)
+        //for(;;)
+        
+        for(int i=0,j=1;i<=10;i++,j=j*2)
+        {
+            System.out.println(i);
+        }
+    }
+    
+}
