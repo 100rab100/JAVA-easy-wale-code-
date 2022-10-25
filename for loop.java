c916d72ebcb75b0eab3b0b7baea191f862af179c
100rab100 <99138605+100rab100@users.noreply.github.com>
2022-10-25 07:34:10 +0530

    Update for loop.java

--- for loop.java
+++ for loop.java
@@ -15,17 +15,16 @@ class first
 
 // second 
 
-package looppractice1;
 
 public class LoopPractice1 {
 
     public static void main(String[] args) {
-        //for(int i=0;i<=10;i++)
-        //for(int i=0;i>0;i--)
+        //for(int i=0;i<=10;i++)  // simple for loop
+        //for(int i=0;i>0;i--)   /// no output
         
         //int i=0;
-        //for(System.out.println("Hi");i<=10;i++)
-        //for(;;)
+        //for(System.out.println("Hi");i<=10;i++)// tula jas vatal tas tu initialize  karu shakto 
+        //for(;;)   // all are optional ...infinite  loop
         
         for(int i=0,j=1;i<=10;i++,j=j*2)
         {
