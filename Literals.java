d7edad3604b178bccd2f684b592244529ac9416e
100rab100 <99138605+100rab100@users.noreply.github.com>
2022-10-26 07:51:30 +0530

    Update Literals.java

--- Literals.java
+++ Literals.java
@@ -23,3 +23,22 @@ public class Literals {
     }
     
 }
+
+
+//integer to binary
+package datatypedetails;
+
+import java.awt.*;
+import javax.swing.*;
+import java.lang.*;
+
+
+public class DataTypeDetails {
+
+    public static void main(String[] args) {
+        int x=-5;//5
+        
+        System.out.println(Integer.toBinaryString(x));
+    }
+    
+}
