73ef8d77664c760e501fe3e3d1e36016309d476d
100rabh patil <99138605+100rab100@users.noreply.github.com>
2022-11-24 17:00:13 +0530

    Update Count Digits Of a Number.java

--- Count Digits Of a Number.java
+++ Count Digits Of a Number.java
@@ -11,3 +11,22 @@ public static void main(String[] args)
        }
         System.out.println(count);
     }
+//
+import java.util.*;
+
+public class Main{
+
+
+
+    public static void main(String[] args) {
+        int count=0;
+        Scanner t=new Scanner(System.in);
+        System.out.println ("Enter a  number ");
+        int num=t.nextInt();
+        String string_num=Integer.toString(num);
+        System.out.println(string_num.length());
+        }
+
+
+    
+}
