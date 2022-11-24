759a422a67e73eddcb4d84ba83f08647d42aad0d
100rabh patil <99138605+100rab100@users.noreply.github.com>
2022-11-24 12:39:28 +0530

    Update postive or negative number.java

--- postive or negative number.java
+++ postive or negative number.java
@@ -19,6 +19,42 @@ public class Main{
             System.out.println ("Zero");
     }
 }
+//Approach 2: Using Integer.signum() Method
+
+//Java Integer class provides an inbuilt function signum() to check if a number is positive or negative. It is a static method that accepts a parameter of integer type.
+
+//It returns 0, if the argument is 0.
+//It returns 1, if the argument>0.
+//It returns -1, if the argument<0.//
+import java.util.*;
+
+public class Main{
+    public static int signump(int x)
+    {
+        int ans = Integer.signum(x);
+        return ans;
+    }
+
+
+    public static void main(String[] args) {
+        
+        Scanner t=new Scanner(System.in);
+        System.out.println ("Enter a  number ");
+        int x=t.nextInt();
+        int num=signump(x);
+
+
+        //Conditions to check if the number is negative or positive
+        if (num > 0)
+            System.out.println ("The number is positive");
+        else if (num < 0)
+            System.out.println ("The number is negative");
+        else
+            System.out.println ("Zero");
+    }
+}
+
+
 
 
 
