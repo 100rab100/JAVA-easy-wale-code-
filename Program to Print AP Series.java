5fb8379bbf1968e665c278c0a3cb4fba310235d9
100rabh patil <99138605+100rab100@users.noreply.github.com>
2022-11-26 08:11:56 +0530

    Update Program to Print AP Series.java

--- Program to Print AP Series.java
+++ Program to Print AP Series.java
@@ -20,3 +20,50 @@
         
     }    
 }
+///Time complexity: O(n) where n is the total number of terms of a given A.P
+
+Auxiliary Space: O(1)
+ 
+ 
+ //
+ 
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+
+
+
+
+            static void printAP(int d, int n, int current) {
+                System.out.print(current + " ");
+
+                if (n == 0)
+                    return;
+
+                printAP(d, n - 1, current + d);
+            }
+
+            public static void main(String[] args) {
+                int a, d, n;
+                Scanner sc = new Scanner(System.in);
+
+                System.out.println("Enter the values of a, d and n : ");
+                a = sc.nextInt();
+                d = sc.nextInt();
+                n = sc.nextInt();
+
+                printAP(d, n - 1, a);
+            }
+       
+
+
+
+}
+
+
+
+
+
